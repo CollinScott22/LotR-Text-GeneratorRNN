de55ca2eb509dbdc0c430f36d3ff247c1f9bf8c7
CollinScott22 <cscott015@gmail.com>
2019-10-29 01:01:02 -0500

    rewrote the code to differ more from the tutorial and work with the illiad instead. I also rewrote the entire input pipeline and some of the buildup before creating the model

--- rnn.py
+++ rnn.py
@@ -1,65 +1,47 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-import tensorflow as tf
+
 import numpy as np
+import pandas as pd
+import pathlib
+import keras
+import matplotlib.pyplot as plt
 import os
-import time
+import datetime
+import tensorflow as tf 
+import tensorflow_datasets as tfds
 
 
+DIRECTORY_URL = 'https://storage.googleapis.com/download.tensorflow.org/data/illiad/'
+FILE_NAMES = ['cowper.txt', 'derby.txt', 'butler.txt']
 
-path = tf.keras.utils.get_file('shakespeare.txt', 'https://storage.googleapis.com/download.tensorflow.org/data/shakespeare.txt')
-text = open(path, 'rb').read().decode(encoding='utf-8')
-print('length of text:: {} characters'.format(len(text)))
+for name in FILE_NAMES:
+	text_dir = tf.keras.utils.get_file(name, origin=DIRECTORY_URL+name)
 
-print(text[:250])
-
-
-vocabulary = sorted(set(text))
-print( '{} unique characters'.format(len(vocab)))
-
-char2idx = {u:i for i, u in enumerate(vocab)}
-idx2char = np.array(vocab)
+parent_dir = os.path.dirname(text_dir)
 
 
-text_as_int = np.array([char2idx[c] for c in text])
+def labeler(example, index):
+	return example, tf.cast(index,tf.int64)
+
+labeled_data_sets = []
 
 
-sequence_length = 200
-epoch_examples = len(text * 2)
+for i, file_name in enumerate(FILE_NAMES):
+	lines_dataset = tf.data.TextLineDataset(os.path.join(parent_dir, file_name))
+	labeled_dataset = lines_dataset.map(lambda ex: labeler(ex, i))
+	labeled_data_sets.append(labeled_dataset)
 
-char_dataset = tf.data.Dataset.from_tensor_slices(text_as_int)
+BUFFER_SIZE = 50000
+BATCH_SIZE = 64
+TAKE_SIZE = 5000
 
+all_labeled_data = labeled_data_sets[0]
+for labeled_dataset in labeled_data_sets[1:]:
+	all_labeled_data = all_labeled_data.concatenate(labeled_dataset)
 
-for i in char_dataset.take(5)
-	print(idx2char[i.numpy()])
+all_labeled_data = all_labeled_data.shuffle(
+	BUFFER_SIZE, reshuffle_each_iteration=False)
 
-sequences = char_dataset.batch(sequence_length+1, drop_remainder=True)
-for item in sequences.take(5):
-	print(repr(''.join(id2char[item.numpy()])))
-
-def split_input_target(chunk):
-	input_text = chunk[:-1]
-	target_text = chunk[:-1]
-	return input_text, target_text
-
-dataset = sequences.map(split_input_target)
-
-
-BATCH_SIZE = 43
-
-
-
-BUFFER_SIZE = 12500
-
-dataset = dataset.shuffle(BUFFER_SIZE).batch(BATCH_SIZE, drop_remainder=True)
-
-dataset
-
-
-
-vocab_size = len(vocab)
-
-embedding_dimensions = 256
-
-
-rnn_units = 2048
+for ex in all_labeled_data.take(5):
+	print(ex)
